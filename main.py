213e15081de541271239f08fe79ada47be17de5b
Sentko <alexgueret50@gmail.com>
2024-07-11 15:27:24 +0200

    main.py reads+prints frankenstein.txt, counts words and returns a unique character count.

---
+++ main.py
@@ -0,0 +1,33 @@
+def main(): 
+    word_count = 0
+    file_contents = ''
+    unique_character_count = {}    
+    #######################################################
+    
+    with open("books/frankenstein.txt") as f: #Read-Prints frankenstein.txt
+        file_contents = f.read()              
+        print(file_contents)                  
+    
+    #######################################################
+    
+    words = file_contents.lower().split()
+    for i in words:
+        word_count += 1
+    print(word_count)
+
+    #######################################################
+    
+    
+    for i in file_contents.lower():
+        if i in unique_character_count:
+            unique_character_count[i] += 1
+        else:
+            unique_character_count[i] = 1
+    print(unique_character_count)
+    return unique_character_count
+    
+        
+
+
+if __name__ == '__main__':
+    main()
